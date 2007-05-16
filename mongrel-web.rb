aa7d85140245fa2764193f6f539d7931a7d71a0a
Stian Haklev <shaklev@gmail.com>
2007-05-17 02:30:24 +0700

    change to using webrick for compatibility. still OK speed with cache.

--- mongrel-web.rb
+++ mongrel-web.rb
@@ -6,51 +6,61 @@
 # Usage:
 # ruby mongrel-web.rb <zdumpfile> <path-prefix>
 
-%w(cgi rubygems mongrel zarchive htmlshrinker).each {|x| require x}
-
+%w(cgi rubygems mongrel zarchive htmlshrinker webrick).each {|x| require x}
+include WEBrick
 Archive = ZArchive::Reader.new(ARGV[0])
 template = Archive.get('__Zdump_Template__')
 Htmlshrink = HTMLExpander.new(template, Archive)
-Cache = {}
-class SimpleHandler < Mongrel::HttpHandler
-  def process(req, resp)
-    resp.start(200) do |head, out|
-      t = Time.now                                    
-      url = ZUtil::url_unescape(req.params['PATH_INFO'][1..-1])
-      url = "index.html" if url.empty?
-      from_cache = false
-    
-      # if style/js
-      if url =~ /(raw|skins|images)\/(.*?)$/
-        url = Regexp::last_match[0]
-        if Cache[url]
-          text = Cache[url] 
-          from_cache = true
-        else
-          text = Archive.get(url)
-          return if text.nil? 
-          line1, line2 = text.split("\n",2) 
-          text = line2 if line1 == 'Unnamed'
-          Cache[url] = text
-        end
-        out.write text
-      else
-        txt = Archive.get(url)
-        txt ||= "Not found\n\nSorry, article #{url} not found" 
-        out.write( Htmlshrink.uncompress(txt) )
-      end
-      head["Content-Type"] = case url
-                                when /\.js$/: "text/javascript"               
-                                when /\.css$/: "text/css"
-                                when /\.html$/: "text/html"
-                                end
-      puts "Got #{url} #{from_cache ? 'from cache ' : ''}in #{"%2.3f" % (Time.now - t)} seconds."
-    end
-  end
-end 
-               
-h = Mongrel::HttpServer.new("0.0.0.0", "2042")
-h.register("/", SimpleHandler.new)
+Cache = {}           
+                                                 
+class NilLog
+  def <<
+  end               
+  def info(*args); end
+  def warn(*args); end
+  def debug(*args); end
+  def debug?(*args); false; end
+end
 
-puts "Webserver started, serving at http://localhost:2042/"
-h.run.join
+wiki_proc = lambda do |req, resp| 
+  resp['Content-Type'] = "text/html"
+  url = ZUtil::url_unescape(req.unparsed_uri[1..-1])
+  t = Time.now                                    
+  url = "index.html" if url.empty?
+  from_cache = ''
+
+  # if style/js
+  if url =~ /(raw|skins|images)\/(.*?)$/
+    url = Regexp::last_match[0]
+    if Cache[url]
+      text = Cache[url] 
+      from_cache = 'from cache '
+    else
+      text = Archive.get(url)
+      return if text.nil? 
+      line1, line2 = text.split("\n",2) 
+      text = line2 if line1 == 'Unnamed'
+      Cache[url] = text
+    end
+    resp.body = text
+  else
+    txt = Archive.get(url)
+    txt ||= "Not found\n\nSorry, article #{url} not found" 
+    resp.body = ( Htmlshrink.uncompress(txt) )
+  end
+  resp["Content-Type"] = case url
+  when /\.js$/: "text/javascript"               
+  when /\.css$/: "text/css"
+  when /\.html$/: "text/html"
+  end
+  puts "Got #{url} #{from_cache}in #{"%2.3f" % (Time.now - t)} seconds."
+end
+
+
+wiki = HTTPServlet::ProcHandler.new(wiki_proc) 
+SERVER = HTTPServer.new(:Port => 2042, :DocumentRoot => Dir.pwd, :Logger => NilLog.new ) 
+
+SERVER.mount("/", wiki) 
+trap("INT"){ SERVER.shutdown;  exit(0) } 
+puts "Server started, website accessible at http://localhost:2042."
+SERVER.start
